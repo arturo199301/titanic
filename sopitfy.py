f0132607f7c0b397f55b6de1e3f0e3bd8e5dbeac
arturo199301 <95254479+arturo199301@users.noreply.github.com>
2026-08-13 17:25:06 -0600

    Update sopitfy.py

--- sopitfy.py
+++ sopitfy.py
@@ -8,7 +8,7 @@ st.image("spotify.png", width=150)
 st.title("🎵 Predicción Completa de Streams")
 
 # 2. Cargar datos
-df = pd.read_csv("most_streamed_spotify_2025.cleaned_v2.csv")
+df = pd.read_csv("most_streamed_spotify_2025_cleaned_v2.csv")
 
 # 3. Definir características (argumentos) y variable a predecir
 features = ['daily_streams', 'daily_streams_rank', 'billed_artist_count', 'is_collaboration']
@@ -24,7 +24,7 @@ model.fit(X, y)
 # 5. Entradas del usuario (Argumentos de entrada)
 daily_streams = st.number_input("Reproducciones diarias:", value=1_500_000, step=100_000)
 rank = st.number_input("Ranking diario (puesto en lista):", min_value=1, value=10)
-artists = st.number_input("Cantidad de artistas en la canción:", min_value=1, max_value=5, value=1)
+artists = st.slider("Cantidad de artistas en la canción:", min_value=1, max_value=5, value=1)
 is_collab = st.checkbox("¿Es una colaboración entre varios artistas?")
 
 # 6. Botón de predicción
