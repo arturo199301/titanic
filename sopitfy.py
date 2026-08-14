17f16e74a229d50700f9bfcf4f973851de5f8d16
arturo199301 <95254479+arturo199301@users.noreply.github.com>
2026-08-13 21:35:10 -0600

    Update sopitfy.py

--- sopitfy.py
+++ sopitfy.py
@@ -16,7 +16,7 @@ st.set_page_config(
 try:
     # Cargar la imagen proporcionada (guardada como 'image_1.png')
     spotify_logo = Image.open('spotify.png')
-    st.image(spotify_logo, width=500)
+    st.image(spotify_logo, width=600)
 except FileNotFoundError:
     st.warning("⚠️ No se encontró la imagen 'image_1.png'. Asegúrate de guardarla en la misma carpeta que app.py.")
 except Exception as e:
